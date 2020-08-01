17732fd73f291913750cbaf4b83f60bd8d3c9978
Aleksa <lukicaleksa04@gmail.com>
2020-08-01 12:12:49 +0200

    Login form html/css

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <script src="assets/node_modules/@fortawesome/fontawesome-free/js/all.min.js"></script>
+</head>
+
+<body>
+    <div class="container login-container">
+        <div class="row">
+            <div class="col-sm"></div>
+            <div class="col-sm login-div">
+                <div class="text-center">
+                    <img src="assets/img/Palacky_University_Olomouc_logo.png" width="300px" alt="" class="img-fluid">
+                </div>
+                <div class="form-div">
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="login-btn">Login</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="text-center">
+                   <h6><a href="">Create account <i class="fa fa-arrow-right"></i></a></h6>
+                </div>
+            </div>
+            <div class="col-sm"></div>
+        </div>
+    </div>
+</body>
+
+</html>
