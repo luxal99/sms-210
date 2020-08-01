7e9fedd48b885fbcb0c63f0223438ebb2faa6f4a
Aleksa <lukicaleksa04@gmail.com>
2020-08-01 17:14:14 +0200

    Added modal for manage roles

--- dashboard.php
+++ dashboard.php
@@ -13,6 +13,12 @@ include "header.php"
                     <div class="col-sm">
                         <img src="assets/img/Palacky_University_Olomouc_logo.png" width="70px" alt="" class="img-fluid">
                     </div>
+                    <div class="col-sm text-center">
+                        <button type="button" class="role-btn" data-toggle="modal" data-target="#exampleModal">
+                            Manage role
+                        </button>
+
+                    </div>
                     <div class="col-sm text-right">
                         <h6>Logged user</h6>
                     </div>
@@ -127,13 +133,13 @@ include "header.php"
                 <div class="student-div" id="student-div">
                     <nav>
                         <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">All subjects</a>
-                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Add</a>
-                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Edit</a>
+                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#all-student" role="tab" aria-controls="nav-home" aria-selected="true">All students</a>
+                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#add-student" role="tab" aria-controls="nav-profile" aria-selected="false">Add</a>
+                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#edit-student" role="tab" aria-controls="nav-contact" aria-selected="false">Edit</a>
                         </div>
                     </nav>
                     <div class="tab-content" id="nav-tabContent">
-                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                        <div class="tab-pane fade show active" id="all-student" role="tabpanel" aria-labelledby="nav-home-tab">
                             <table class="table">
                                 <thead>
                                     <tr>
@@ -165,7 +171,7 @@ include "header.php"
                                 </tbody>
                             </table>
                         </div>
-                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                        <div class="tab-pane fade" id="add-student" role="tabpanel" aria-labelledby="nav-profile-tab">
                             <form style="padding: 2em;">
                                 <div class="form-group">
                                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -177,7 +183,7 @@ include "header.php"
                                 <button type="submit" class="add-subject-btn">Save</button>
                             </form>
                         </div>
-                        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                        <div class="tab-pane fade" id="edit-student" role="tabpanel" aria-labelledby="nav-contact-tab">
                             <div style="padding: 2em;">
                                 <h3 class="subject-h3">Choose subject for edit</h3>
                                 <div class="dropdown show">
@@ -204,8 +210,40 @@ include "header.php"
                         </div>
                     </div>
                 </div>
-                
 
+
+            </div>
+            <!-- Button trigger modal -->
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+
+                        <div class="modal-body">
+                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Add</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Manage roles</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content" id="pills-tabContent">
+                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                    <form style="padding: 2em;"> 
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                        </div>
+                                        <button type="submit" class="add-role-btn">Save</button>
+                                    </form>
+                                </div>
+                                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">..2.</div>
+                                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">..1.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
