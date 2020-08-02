a4f8b6909b933eb725fb94652e95cabd39f49052
Aleksa <lukicaleksa04@gmail.com>
2020-08-02 20:34:53 +0200

    Create user.php page

---
+++ user.php
@@ -0,0 +1,15 @@
+<?php
+include "header.php"
+?>
+
+<link rel="stylesheet" href="assets/css/user.css">
+
+<body>
+    <div class="user">
+        <div class="user-container">
+            <div class="text-left">
+                <h4>Logged user</h4>
+            </div>
+        </div>
+    </div>
+</body>
