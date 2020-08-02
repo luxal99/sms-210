dbcc9462223e51df419f56b7915cc761d097105e
Aleksa <lukicaleksa04@gmail.com>
2020-08-02 23:38:07 +0200

    Project finish

--- user.php
+++ user.php
@@ -10,6 +10,41 @@ include "header.php"
             <div class="text-left">
                 <h4>Logged user</h4>
             </div>
+            <div class="container">
+                <h3>My exams</h3>
+                <div class="table-div">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </div>
     </div>
 </body>
